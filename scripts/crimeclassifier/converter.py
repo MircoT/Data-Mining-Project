044b26a3618d237a8448e4b4ebc468553111df2b
MircoT <mirco.theone@gmail.com>
2016-09-06 23:04:09 +0200

    Add generation of set value in selected features

--- scripts/crimeclassifier/converter.py
+++ scripts/crimeclassifier/converter.py
@@ -6,6 +6,31 @@ from os.path import dirname
 __all__ = ['to_bin']
 
 
+def get_fix_range_value(tot_elms, index):
+    """Percentage value of the given index in a set of elements.
+
+    The value returned is in the middle:
+
+    For example:
+
+    0.0               1.0
+     |-----|-----|-----|
+        ^     ^     ^
+
+    Params:
+        tot_elems (int): number of elements in the set
+        index (int): the index of the current element,
+                     starting from 1
+
+    Return:
+        float: percentage with dot notation, 
+               so from 0.0 to 1.0
+
+    """
+    step = (1. / tot_elms) / 2.
+    return float((index / tot_elms) - step)
+
+
 def to_bin(report_f, config_f):
     """Convert a dataset from CSV to binary.
 
@@ -42,13 +67,53 @@ def to_bin(report_f, config_f):
     else:
         selected_features = config.get('features')
 
-    if config.get('feature_class_name', '') not in report['features']:
+    if config.get('feature_class_name', '') not in selected_features:
         raise Exception(
             "Feature class name not valid or not exists")
 
+    for feature in selected_features:
+        if report['features'][feature]['type'] == 'string':
+            report['features'][feature]['p_vals'] = {}
+            if config.get('scaled_sets', False):
+                ##
+                # Is not the maximum but the middle of the range
+                #
+                # For example:
+                #
+                # 0.0                 1.0
+                #  |-----|---------|---|
+                #     ^       ^      ^
+                #
+                cur_val = report['num_records']
+                for name, count in report['features'][feature]['set']:
+                    report['features'][feature]['p_vals'][name] = float(
+                        (cur_val - count / 2) / report['num_records'])
+                    cur_val -= count
+            else:
+                ##
+                # Like the scaled solution but with fixed
+                # ranges
+                #
+                # For example:
+                #
+                # 0.0               1.0
+                #  |-----|-----|-----|
+                #     ^     ^     ^
+                tot_elms = len(report['features'][feature]['set'])
+                for index, (name, count) in enumerate(reversed(
+                        report['features'][feature]['set']), 1):
+                    report['features'][feature]['p_vals'][
+                        name] = get_fix_range_value(tot_elms, index)
+
+            ##
+            # DEBUG
+            # print(list(reversed(
+            #     sorted(report['features'][feature]['p_vals'].items(),
+            #            key=lambda elm: elm[1]))))
+
     crimes = []
 
-    ## ----- TO DO -----
+    # ----- TO DO -----
     # for num, row in enumerate(read_csv(filename), 1):
     #     msg_load.show("> Parsed {} of {}".format(num, report['num_records']))
 
